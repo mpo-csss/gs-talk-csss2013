f1c0ee208ca362d1c46b87fba8fc3f07fab71f54
sbalev <stefan.balev@gmail.com>
2013-07-13 12:12:46 +0200

    Added tag exchange graph

---
+++ workspace/demos/src/org/graphstream/demo/sailing/TagGraph.java
@@ -0,0 +1,28 @@
+package org.graphstream.demo.sailing;
+
+import java.io.IOException;
+
+import org.graphstream.graph.ElementNotFoundException;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.stream.GraphParseException;
+
+
+public class TagGraph {
+
+	public static void main(String[] args) throws ElementNotFoundException, IOException, GraphParseException {
+		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+		Graph graph = new MultiGraph("Tag Exchange");
+		graph.read("data/tag_exchange.dgs");
+		graph.addAttribute("ui.quality");
+		graph.addAttribute("ui.antialias");
+		graph.addAttribute("ui.stylesheet", "url('data/style_tag.css')");
+		graph.display();
+		
+		System.out.printf("%10s%10s%10s%10s\n", "Boat", "Received", "Sent", "Balance");
+		for (Node node : graph) {
+			System.out.printf("%10s%10d%10d%10d\n", node.getId(), node.getInDegree(), node.getOutDegree(), node.getInDegree() - node.getOutDegree());
+		}
+	}
+}
