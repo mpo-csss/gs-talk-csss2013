1fce6ba65edf6ae66e6f9d4eec4e66fab39299cb
sbalev <stefan.balev@gmail.com>
2013-07-15 11:46:10 +0200

    Possibilty to fix a viewport

--- workspace/demos/src/org/graphstream/demo/sailing/SailingDGSViewer.java
+++ workspace/demos/src/org/graphstream/demo/sailing/SailingDGSViewer.java
@@ -6,18 +6,22 @@ import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceDGS;
+import org.graphstream.ui.swingViewer.Viewer;
+import org.graphstream.ui.swingViewer.util.Camera;
 
 public class SailingDGSViewer {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		Graph graph = new SingleGraph("Sailing");
-		graph.display(false);
+		Viewer v = graph.display(false);
+//		Camera c = v.getDefaultView().getCamera();
+//		c.setGraphViewport(-1700, -700, 1700, 700);
 		
 		FileSource source = new FileSourceDGS();
 		source.addSink(graph);
 		source.begin("data/tracks.dgs");
 		while (source.nextStep()) {
-			Thread.sleep(1);
+			Thread.sleep(5);
 			// do something here
 		}
 	}
